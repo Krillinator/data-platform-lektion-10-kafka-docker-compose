4994f511b75f9ecb8ad7efe19d272e6394784f73
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2026-02-25 19:29:56 +0100

    Fix: Posting product would throw error due to missing params

--- app/main.py
+++ app/main.py
@@ -48,20 +48,22 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/products", status_code=status.HTTP_201_CREATED, response_model=ProductRead)
-def post_product(product: ProductSchema) -> ProductSchema:
+def post_product(product: ProductSchema):
 
     with app.state.pool.connection() as conn:
-        with conn.transaction():
-            conn.execute(
+        with conn.cursor(row_factory=dict_row) as cur:
+            cur.execute(
                 """
                 INSERT INTO products (name, price, quantity)
                 VALUES (%s, %s, %s)
-                RETURNING id, created_at
+                RETURNING id, name, price, quantity, created_at
                 """,
                 (product.name, product.price, product.quantity),
-            ).fetchone()
+            )
+            row = cur.fetchone()
+            conn.commit()
 
-    return product
+    return row
 
 @app.get("/products/{product_id}", response_model=ProductRead)
 def get_product(product_id: int):
